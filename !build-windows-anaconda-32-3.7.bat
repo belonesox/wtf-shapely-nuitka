2cd07514280b169139d3c2e2699846b8f54ff6e9
Stas Fomin <stas-fomin@yandex.ru>
2020-04-16 03:53:27 +0300

    troubles with windows anaconda 32/64

---
+++ !build-windows-anaconda-32-3.7.bat
@@ -0,0 +1,4 @@
+﻿rem
+set PATH=%PATH%;C:\anaconda32-3.7;C:\anaconda32-3.7\Scripts;
+set NUITKA_CLCACHE_BINARY=C:\anaconda32-3.7\Scripts\clcache.exe
+python -m nuitka --recurse-none --standalone shapely-wtf.py --verbose --jobs=4  --recurse-to=shapely --include-package=shapely  --output-dir=w32-wtf
---
+++ shapely-wtf.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+ 
+from shapely.geometry.polygon import Polygon
+
+if __name__ == '__main__':
+   print('11111')
